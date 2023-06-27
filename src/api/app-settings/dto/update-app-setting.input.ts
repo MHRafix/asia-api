0a1c61fe0910873302afdfb88ad15c4a950529cc
MHRafix <fmhrafiz27@gmail.com>
2023-06-27 12:08:10 +0600

    fixed id issues

--- src/api/app-settings/dto/update-app-setting.input.ts
+++ src/api/app-settings/dto/update-app-setting.input.ts
@@ -1,8 +1,8 @@
+import { Field, InputType, PartialType } from '@nestjs/graphql';
 import { CreateAppSettingInput } from './create-app-setting.input';
-import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateAppSettingInput extends PartialType(CreateAppSettingInput) {
   @Field(() => String)
-  id: string;
+  _id: string;
 }
