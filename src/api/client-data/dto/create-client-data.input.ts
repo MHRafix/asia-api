dad0e29cf106f8c870de8852d9f4fd4fb821f635
MHRafix <fmhrafiz27@gmail.com>
2024-07-27 22:36:36 +0600

    entity fixed

--- src/api/client-data/dto/create-client-data.input.ts
+++ src/api/client-data/dto/create-client-data.input.ts
@@ -2,26 +2,6 @@ import { Field, ID, InputType } from '@nestjs/graphql';
 import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
 
 @InputType()
-export class SocialInfo {
-  @Field(() => String)
-  @IsNotEmpty()
-  @IsEmail()
-  public email: string;
-
-  @Field(() => String, { nullable: true })
-  @IsOptional()
-  public facebook: string;
-
-  @Field(() => String, { nullable: true })
-  @IsOptional()
-  public linkedin: string;
-
-  @Field(() => String, { nullable: true })
-  @IsOptional()
-  public phone: string;
-}
-
-@InputType()
 export class CreateClientDataInput {
   @Field(() => ID, { nullable: true })
   _id: string;
